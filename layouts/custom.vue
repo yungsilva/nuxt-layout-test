206ce68623066b870b92a3853441d9f0a57076ee
Yung Silva <yung@cataline.io>
2022-05-30 16:59:05 -0300

    first commit

---
+++ layouts/custom.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="custom-layout">
+    <slot />
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="default-layout">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+definePageMeta({
+  // middleware: ['auth']
+})
+</script>
